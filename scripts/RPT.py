6816f38ff7cf7e2914a7a98f720c615f673d45be
Konnor Petersen <konnorjp@gmail.com>
2019-01-22 12:59:02 -0700

    Created file for phonetic transcription

---
+++ scripts/RPT.py
@@ -0,0 +1,29 @@
+import re
+
+vowels = 'аоэуияёеюы'
+vow_ak = 'ао'
+consonants = ''
+
+def softness(word) {
+    word = re.sub(r'(?<=(\s|[' + vowels + 'ьъ]))ю','йу',word)
+    word = re.sub(r'(?<=(\s|[' + vowels + 'ьъ]))е','йэ',word)
+    word = re.sub(r'(?<=(\s|[' + vowels + 'ьъ]))я','йа',word)
+    word = re.sub(r'(?<=(\s|[' + vowels + 'ьъ]))ё','йо',word)
+    word = re.sub(r'(?<=[ьъ])и','йи',word)
+
+    word = re.sub(r'ь','\'',word)
+    word = re.sub(r'ъ','',word)
+    word = re.sub(r'ю','\'у',word)
+    word = re.sub(r'я','\'а',word)
+    word = re.sub(r'е','\'э',word)
+    word = re.sub(r'ё','\'о',word)
+
+    '''(?<![жчщшцй\s])и'''
+}
+
+
+def akanje() {
+    akanje1 = r'[' + vow_ak + '](?!́)(?=(?=.*[' + vowels + '].*(о́|а́))|\b)'
+    akanje2 = r'[оа](?!́)(?=[^оа]*(о́|а́))'
+
+}
