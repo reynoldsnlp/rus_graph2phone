4132fc07104a44613b6f54d823cce4af702f3b3f
Konnor <konnorjp@gmail.com>
2019-05-29 13:08:23 -0600

    Fixed twolc file, began setting up file to transcribe across word boundaries

--- scripts/hfst-compiler.sh
+++ scripts/hfst-compiler.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ test.csv -nt /tmp/g2p_lex-test.txt ]]; then
+#if [[ test.csv -nt /tmp/g2p_lex-test.txt ]]; then
 	echo "creating tmp test-pair file..."
 	cat test.csv | cut -d "," -f 1 > /tmp/g2p_lex-test.txt
-fi
+#fi
 
 # if source is newer than compiled, recompile
-if [[ g2p.twolc -nt g2p.hfst ]]; then
+#if [[ g2p.twolc -nt g2p.hfst ]]; then
 	echo "compiling hfst file..."
 	hfst-twolc -i g2p.twolc -o g2p.tmp.hfst
 	echo "creating base FST with universal language..."
@@ -15,7 +15,7 @@ if [[ g2p.twolc -nt g2p.hfst ]]; then
 	hfst-compose-intersect univ.hfst g2p.tmp.hfst > g2p.hfst
 	echo "converting to optimized lookup format..."
 	hfst-fst2fst -O -i g2p.hfst -o g2p.hfstol
-fi
+#fi
 
 echo "running test entries..."
 cat /tmp/g2p_lex-test.txt | hfst-lookup g2p.hfstol 2>/dev/null >/tmp/output_g2p_hfst.txt
